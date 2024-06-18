55be4efc7fbb3a5c43a6946992ad97f0d6f04aa6
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-06-18 21:49:18 +0000

    finished fibonacci

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function fibonacciSequence() {
+  const integer = parseInt(document.getElementById('num-entered').value)
+  let answer = 0
+  let fibSeq = [0, 1] // initialize the Fibonacci sequence with the first two numbers
+
+  for (let counter = 2; counter <= integer; counter++) {
+    fibSeq.push(fibSeq[counter - 1] + fibSeq[counter - 2]) // calculate the next number in the sequence
+  }
+
+  document.getElementById('answer').innerHTML = `The Fibonacci sequence up to ${integer} is: ${fibSeq.join(', ')}` // display the result
+}
